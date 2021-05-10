7db1c10e35480a95bfc36e490eb34cc060b056bf
princekennedy <prince0888407189@gmail.com>
2021-05-10 18:54:12 +0200

    update

--- resources/js/Pages/places/index.vue
+++ resources/js/Pages/places/index.vue
@@ -6,7 +6,7 @@
                 <div class="rounded m-3 p-2">
                     <h5 class="font-bold uppercase text-gray-600  p-2">
                         DEPO LIST
-                        <span class=" text-green-400 hover:text-green-600 hover:font-medium fa fa-plus float-right cursor-pointer"> Add </span>
+                        <span class=" text-primary hover:text-primary hover:font-medium fa fa-plus float-right cursor-pointer"> Add </span>
                     </h5>
                 </div>
             </div>
@@ -17,14 +17,14 @@
                     <div class="bg-white border rounded shadow p-2">
                         <div class="flex flex-row items-center">
                             <div class="flex-shrink pr-4">
-                                <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div>
+                                <div class="rounded p-3 bg-primary"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div>
                             </div>
                             <div class="flex-1 text-right md:text-center">
                                 <h5 class="font-bold uppercase text-gray-500">
                                      {{ place.name }}
                                 </h5>
                                 <h3 class="font-bold text-3xl">  
-                                    {{ place.users.length }} <span class="text-green-500"><i class="fas fa-caret-up"></i></span>
+                                    {{ place.users.length }} <span class="text-primary"><i class="fas fa-caret-up"></i></span>
                                 </h3>
                             </div>
                         </div>
